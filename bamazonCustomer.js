c47a6620129d658bb5b3d01e3c90c665fa99d6a8
Barrett <Aliciawyse@users.noreply.github.com>
2017-11-04 00:09:01 -0400

    wip

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 
 //create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -15,14 +17,45 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     if (err) throw err;
-    console.log("Connected as ID " + connection.threadID + "\n");
     afterConnection();
 });
+
 
 function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
+        displayDB();
+        start();
         connection.end();
     });
 }
+
+function displayDB(){
+    var query = "SELECT * FROM products";
+
+    connection.query(query, function (err,res) {
+        for (var i = 0; i < res.length; i++){
+            console.log(res[i].item_id + ") " + res[i].product_name + " // " + "$" +res[i].price);
+        }
+    });
+}
+
+function start(){
+    inquirer
+        .prompt([
+            {
+                name: "buying",
+                type: "input",
+                message: "What's the ID of the product that you'd like to buy?\n"
+            },
+            {
+                name: "amount",
+                type: "input",
+                message: "Great choice! How many would you like?"
+            }
+        ])
+        .then(function(response){
+
+    })
+}
+
